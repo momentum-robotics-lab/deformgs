47e5d5399f3c96ace8e2a8be461f232b65421835
Duisterhof <bduister@andrew.cmu.edu>
2024-02-23 13:19:54 -0500

    scripts update bg scenes

--- run_scripts/render_all_bg.sh
+++ run_scripts/render_all_bg.sh
@@ -12,12 +12,12 @@ export SCENE_6="scene_6"
 
 port=6027 
 #for SCENE in $SCENE_1 $SCENE_2 $SCENE_3 $SCENE_4 $SCENE_5 $SCENE_6;
-for SCENE in $SCENE_1 $SCENE_2 $SCENE_3 $SCENE_4 $SCENE_5 $SCENE_6;
+for SCENE in $SCENE_1 ;
 do
     for isometry in $ISOMETRY;
     do 
 
-        python3 render_experimental.py --model_path "output/final_scenes_no_bg_exp_iso/${SCENE}" --configs arguments/mdnerf-dataset/cube.py --skip_train --skip_video --view_skip 50 --time_skip 1 --log_deform \
+        python3 render_experimental.py --model_path "output/final_scenes_bg_20m_range_exp_iso/${SCENE}" --configs arguments/mdnerf-dataset/cube.py --skip_train --skip_video --view_skip 50 --time_skip 1 --log_deform \
         --show_flow --flow_skip 2 --tracking_window 100 --scale 1
         # add one to port
         port=$((port+1))
--- run_scripts/run_all_bg.sh
+++ run_scripts/run_all_bg.sh
@@ -11,14 +11,14 @@ export SCENE_5="scene_5"
 export SCENE_6="scene_6"
 
 port=6027 
-for SCENE in $SCENE_4 $SCENE_5;
+for SCENE in $SCENE_1;
 #for SCENE in $SCENE_1;
 do
     for isometry in $ISOMETRY;
     do 
-        python3 train.py -s "data/final_scenes/${SCENE}" --port $port --expname "final_scenes_no_bg_exp_iso/${SCENE}" --configs arguments/mdnerf-dataset/cube.py --lambda_w 100000 \
+        python3 train.py -s "data/final_scenes_bg/${SCENE}" --port $port --expname "final_scenes_bg_20m_range_exp_iso/${SCENE}" --configs arguments/mdnerf-dataset/cube.py --lambda_w 100000 \
         --lambda_rigidity $RIGIDITY_LAMBDA --lambda_spring $LAMBDA_SPRING  --lambda_momentum 0.1 \
-        --use_wandb --wandb_project "final_${SCENE}"  --wandb_name "2.6m_range_iso_exp_no_bg" --k_nearest 5 --lambda_isometric $isometry --time_skip 1
+        --use_wandb --wandb_project "final_${SCENE}_bg"  --wandb_name "iso_exp_bg" --k_nearest 5 --lambda_isometric $isometry --time_skip 1
         # add one to port
         port=$((port+1))
     done
